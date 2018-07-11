89c0727289dc2ae96e6091bac77075140a2bc1bd
Eveng Thao <ethao@neptunetg.com>
2018-07-11 09:18:36 -0400

    first commit

---
+++ SQLParsing/SearchForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace SQLParsing
+{
+    partial class SearchForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.txtSearchText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Location = new System.Drawing.Point(12, 97);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(1191, 424);
+            this.txtOutput.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Location = new System.Drawing.Point(1128, 527);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Run";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(13, 13);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(636, 20);
+            this.txtPath.TabIndex = 2;
+            this.txtPath.Text = "c:\\temp\\initialdata.sql";
+            // 
+            // txtSearchText
+            // 
+            this.txtSearchText.Location = new System.Drawing.Point(12, 39);
+            this.txtSearchText.Name = "txtSearchText";
+            this.txtSearchText.Size = new System.Drawing.Size(636, 20);
+            this.txtSearchText.TabIndex = 2;
+            this.txtSearchText.Text = "SearchText";
+            // 
+            // SearchForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1215, 562);
+            this.Controls.Add(this.txtSearchText);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtOutput);
+            this.Name = "SearchForm";
+            this.Text = "Search Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.TextBox txtSearchText;
+    }
+}
+
---
+++ SQLParsing/SearchForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQLParsing
+{
+    public partial class SearchForm : Form
+    {
+        public SearchForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+           
+            string[] lines = System.IO.File.ReadAllLines(@"c:\temp\initialdata.sql");
+
+            // Display the file contents by using a foreach loop.
+            System.Console.WriteLine("Contents of WriteLines2.txt = ");
+            var sqlStatement = new StringBuilder();
+            var endOfSqlStatement = false;
+            var containsSearchText = false;
+            txtOutput.Clear();
+            foreach (string line in lines)
+            {
+                if (line.ToLower().Contains(txtSearchText.Text.ToLower()))
+                {
+                    containsSearchText = true;
+                }
+
+                if (containsSearchText)
+                {
+                    sqlStatement.Append(line.Replace("\r\n", "").Trim());
+                    if (line.Contains(";"))
+                    {
+                        Console.WriteLine(sqlStatement.ToString() + "\r\n");
+                        txtOutput.AppendText(sqlStatement.ToString() + "\r\n");
+                        txtOutput.AppendText("\r\n");
+                        sqlStatement.Clear();
+                        containsSearchText = false;
+                    }
+                }
+              
+
+                // Use a tab to indent each line of the file.
+                //Console.WriteLine("\t" + line);
+            }
+
+            // Keep the console window open in debug mode.
+        }
+    }
+}
